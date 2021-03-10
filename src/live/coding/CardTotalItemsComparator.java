464449f6fb5697dabf4106df99a3a051c6dc0bc5
fhc02hpg <fhc02hpg@users.noreply.github.com>
2021-03-10 19:06:01 +0100

    missing cart example from LV

---
+++ src/live/coding/CardTotalItemsComparator.java
@@ -0,0 +1,12 @@
+package live.coding;
+
+import java.util.Comparator;
+
+public class CardTotalItemsComparator implements Comparator<Cart> {
+
+  @Override
+  public int compare(Cart o1, Cart o2) {
+    return Integer.compare(o2.getTotalItems(),o1.getTotalItems());
+  }
+
+}
---
+++ src/live/coding/CartSortDemo.java
@@ -0,0 +1,41 @@
+package live.coding;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CartSortDemo {
+
+  public static void main(String[] args) {
+
+    Cart c1 = new Cart("john",4,10,125.90);
+    Cart c2 = new Cart("jane",1,1,5.90);
+    Cart c3 = new Cart("jennifer",10,10,1000.00);
+    Cart c4 = new Cart("bob",3,9,472.50);
+    Cart c5 = new Cart("peter",7,5,333.60);
+
+    List<Cart> carts = new ArrayList<>();
+    carts.add(c1);
+    carts.add(c2);
+    carts.add(c3);
+    carts.add(c4);
+    carts.add(c5);
+
+    System.out.println("pre-sort: "+carts);
+
+    Collections.sort(carts);
+
+    System.out.println("post-sort: "+carts);
+
+    Collections.sort(carts,new CartUsernameComparator());
+
+    System.out.println("username comparator sort: "+carts);
+
+    carts.sort(new CardTotalItemsComparator());
+
+    System.out.println("total items comparator sort: "+carts);
+
+
+  }
+
+}
